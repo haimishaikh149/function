d746da186ed9b850252195bfb5dc6aae3dc3c4df
haimishaikh149 <160590123+haimishaikh149@users.noreply.github.com>
2024-04-25 20:13:06 +0500

    Add files via upload

---
+++ fs.js
@@ -0,0 +1,22 @@
+// simple function
+//  function name0(){
+//     console.log("assalam u alaikum")
+//  }
+//  name0()
+//  name0()
+//  function name2(){
+//     console.log("assalam u alaikum")
+//     console.log("how are you")
+//  }
+//  name2()
+//  name2()
+//  function name1(){
+//       console.log(`assalam u alaikum "${"every one"}"`)
+//  }
+//  name1()
+//  name1()
+function engVocabilry(Name1) {
+    console.log("Name1");
+}
+engVocabilry("who");
+engVocabilry("where");
---
+++ fs.ts
@@ -0,0 +1,54 @@
+         // simple function
+
+
+        //  function name0(){
+        //     console.log("assalam u alaikum")
+            
+        //  }
+        //  name0()
+        //  name0()
+           
+
+        //  function name2(){
+        //     console.log("assalam u alaikum")
+        //     console.log("how are you")
+        //  }
+        //  name2()
+        //  name2()
+         
+
+        //  function name1(){
+        //       console.log(`assalam u alaikum "${"every one"}"`)
+        //  }
+        //  name1()
+        //  name1()
+
+// function engVocabilry(Name1:string){
+//    console.log(Name1)
+// }
+// engVocabilry("who")
+// engVocabilry("where")
+
+
+
+
+
+
+        // function userInfo(favDish:string,favjuice:string){
+        //     console.log(`my favourite dish is "${"favDish"}" , my favourite drink is "${"favjuice"}"`)
+        //      }
+        //  userInfo("zinger" , "blue berry juice")
+        //  userInfo("biryani" , "7up") 
+        //  userInfo("chips" , "orange juice")
+
+        // function books(Best1:string = "english" , Best2:string = "pencile"){
+        //    console.log(Best1 , Best2)
+
+        // }
+        // books()
+        // books("accounting" , "advance accounting")
+        // books("computer science" , "chemistry")
+
+
+        
+
